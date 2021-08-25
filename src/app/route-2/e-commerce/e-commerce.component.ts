bf1726438813d1751f41f99c31ef17210c62db3c
Rakshit Kapoor <rakkapoo1@publicisgroupe.net>
2021-08-25 16:19:30 +0530

    route-2 exercise done

--- src/app/route-2/e-commerce/e-commerce.component.ts
+++ src/app/route-2/e-commerce/e-commerce.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ECommerceComponent implements OnInit {
 
+  products: any[] = [];
+  originalProducts: any[] = [];
+  selectedView: string = "grid";
+  priceFilter: string = "none";
+
   constructor() { }
 
   ngOnInit(): void {
+    let productCount = 40;
+    for(let i=0; i<productCount; i++) {
+      this.products.push(
+        {
+          "index": i+1,
+          "price": this.getRandomIntInclusive(1000,5000)
+        }
+      );
+    }
+
+    this.originalProducts = [...this.products];
+  }
+
+  getRandomIntInclusive(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
+  changeView(view: any) {
+    this.selectedView = view;
+  }
+
+  applyFilter() {
+    if(this.priceFilter == "lowToHigh") {
+      this.products.sort((item1, item2)=> {
+        return item1.price - item2.price;
+      });
+    }
+    else if(this.priceFilter == "highToLow") {
+      this.products.sort((item1, item2)=> {
+        return item2.price - item1.price;
+      });
+    }
+    else {
+      this.products = [...this.originalProducts];
+    }
   }
 
 }
--- src/app/route-2/route-2.module.ts
+++ src/app/route-2/route-2.module.ts
@@ -4,14 +4,17 @@ import { CommonModule } from '@angular/common';
 import { Route2RoutingModule } from './route-2-routing.module';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
 
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     ECommerceComponent
   ],
   imports: [
     CommonModule,
-    Route2RoutingModule
+    Route2RoutingModule,
+    MatButtonModule,
+    MatSelectModule
   ]
 })
 export class Route2Module { }
